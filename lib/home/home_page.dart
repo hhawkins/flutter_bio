a0b3d4f5946c0726b0bcdac4d1756538497d4681
Hamza Hawkins <hhawkins@users.noreply.github.com>
2020-01-08 19:46:44 -0500

    Small update

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -35,35 +35,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-
-//import 'package:flutter/material.dart';
-/*
-class MyTabController extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-          length: 3,
-          child: Scaffold(
-            appBar: AppBar(
-              bottom: TabBar(
-                tabs: [
-                  Tab(icon: Icon(Icons.directions_car)),
-                  Tab(icon: Icon(Icons.directions_transit)),
-                  Tab(icon: Icon(Icons.directions_bike)),
-                ],
-              ),
-              title: Text('Tabs Demo'),
-            ),
-            body: TabBarView(
-              children: [
-                Icon(Icons.directions_car),
-                Icon(Icons.directions_transit),
-                Icon(Icons.directions_bike),
-              ],
-            ),
-          ),
-    );
-  }
-}
-*/
